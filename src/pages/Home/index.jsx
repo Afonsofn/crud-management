99756a4eeca5f35d542792357ab81bbf21324964
afonso <afonsoafn@gmail.com>
2021-06-15 09:23:05 -0300

    novas features

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,46 @@
-import './style.css';
+import DeleteConfirmation from '../../components/DeleteConfirmation';
+import { useEffect } from 'react';
+import M from "materialize-css";
+import './style.scss';
 
 function Home() {
+
+  useEffect(() => {
+    M.Modal.init(document.querySelectorAll('.modal-delete-confirmation'))
+  }, [])
+
   return (
-    <div className="App">
-      Home
+    <div className="table-container">
+      <a class="waves-effect waves-light btn new-employee" ><i class="material-icons left">add</i>new employee</a>
+      <table className="striped responsive-table">
+        <thead>
+          <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Start Date</th>
+              <th>Team</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Alvin</td>
+            <td>AlvinEclair@gmail.com</td>
+            <td>19/11/1997</td>
+            <td>Front-End Developer</td> 
+            <td className="button-td">
+              <div className="button-wrapper">
+                <a class="waves-effect waves-light btn-small">
+                  <i class="material-icons ">mode_edit</i>
+                </a>
+                <a class="waves-effect waves-light btn-small red modal-trigger lighten-2" href="#modal-delete">
+                  <i class="material-icons">clear</i>
+                </a>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <DeleteConfirmation />
     </div>
   );
 }
